8157ce10845e1607d4d3ec9a3c10e7edff8cd2df
Sanjar Kodirov <kodirov.sanzhar@bk.ru>
2022-02-05 00:26:24 +0500

    filter by tags

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,7 +1,7 @@
 import Reactk, { useState, useContext, useEffect } from 'react';
 import { Context } from '../../context';
 const Search = () => {
-  const { photos, setPhotos, search, setSearch } = useContext(Context);
+  const { setSearch, setTags } = useContext(Context);
 
   return (
     <div className='search-cont container'>
@@ -10,6 +10,18 @@ const Search = () => {
         class='input primary'
         placeholder='search by name'
       />
+
+      <div class='select'>
+        <select
+          onChange={(e) => setTags(e.target.value)}
+          className='input primary'
+        >
+          <option value='posts'>Posts</option>
+          <option value='todos'>Todos</option>
+          <option value='albums'>Albums</option>
+          <option value='photos'>Photos</option>
+        </select>
+      </div>
     </div>
   );
 };
--- src/context.js
+++ src/context.js
@@ -5,17 +5,20 @@ const Context = createContext();
 const ContextProvider = ({ children }) => {
   const [photos, setPhotos] = useState([]);
   const [isLoading, setIsloading] = useState(true);
+  /* search */
   const [search, setSearch] = useState('');
-
+  const [tags, setTags] = useState('photos');
   const getPhotos = async () => {
-    const response = await fetch('https://jsonplaceholder.typicode.com/photos');
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/${tags}`
+    );
     const data = await response.json();
     setPhotos(data);
     setIsloading(false);
   };
   useEffect(() => {
     getPhotos();
-  }, []);
+  }, [tags]);
   return (
     <Context.Provider
       value={{
@@ -25,6 +28,7 @@ const ContextProvider = ({ children }) => {
         search,
         setSearch,
         getPhotos,
+        setTags,
       }}
     >
       {children}
