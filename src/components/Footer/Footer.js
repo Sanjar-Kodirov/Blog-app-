5d8da6daa769643a76271a83df69fb24cd0e10e3
Sanjar Kodirov <kodirov.sanzhar@bk.ru>
2022-02-04 23:15:36 +0500

    modal completed

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className='footer'>
+      <div className='footer-content'>
+        <h2>Stay in Touch</h2>
+        <span className='line'></span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
